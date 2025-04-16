18d0f17ed3e0acc7cb71f8a2c7c1f410f6360d5d
woomike35 <michaeleze226@gmail.com>
2025-04-16 13:34:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 
-
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
 
-    print(f'The random values are {numbers}')
-    print(f'The total is {total}')
+    while True:
+        num = random.randint(1, 10)
+        if total + num > 100:
+            break
+        numbers.append(num)
+        total += num
+
+    # Now add the last number that caused the sum to exceed 100 to the list
+    numbers.append(num)
+
+    print(' '.join(str(n) for n in numbers))  # Print random values on one line
+    print(f'The total sum is {total}')        # Sum excluding last number
 
     ########################################
     # Do not delete the return statement
